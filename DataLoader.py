ffbf4f67d9a099e18c9045ba3b4ffe5fd5268660
joohoshin <jooho@boozaapp.com>
2023-03-19 15:51:41 +0900

    20230319

--- DataLoader.py
+++ DataLoader.py
@@ -1,6 +1,12 @@
 # pandas 데이터리더
 # https://pydata.github.io/pandas-datareader/stable/index.html
+
+# 상장기업 목록, 엑셀다운로드
+# https://kind.krx.co.kr/corpgeneral/corpList.do?method=loadInitPage
+
+
 import pandas_datareader.data as web
+import pandas as pd
 
 # 데이터 불러오는 함수
 def korea_stock(code, start, end):
@@ -8,5 +14,12 @@ def korea_stock(code, start, end):
     df = df.astype('int') # 정수로 변환
     return df
 
+def kospi_list():
+    kospi = pd.read_csv('data/KOSPI-20230319.csv', encoding = 'cp949')
+    kospi['종목코드'] = kospi['종목코드'].astype('str') #문자로 변경
+    return kospi
 
-
+def kosdaq_list():
+    kosdaq = pd.read_csv('data/KOSDAQ-20230319.csv', encoding = 'cp949')
+    kosdaq['종목코드'] = kosdaq['종목코드'].astype('str')
+    return kosdaq
--- main.py
+++ main.py
@@ -1,5 +1,44 @@
 import DataLoader as dl
 
-# 실행 스크립트
-shinsung_df = dl.korea_stock('065350', '2020-01-01', '2023-03-12')   
+
+shinsung_df = dl.korea_stock('065350', '2020-01-01', '2023-03-12')
 shinsung_df.Close.plot()
+
+kospi = dl.kospi_list()
+kosdaq = dl.kosdaq_list()
+
+code = kospi['종목코드'][0]
+dl.korea_stock(code, '2020-01-01', '2023-03-18')
+
+kospi_list = []
+
+for i in range(0,10):
+    code = kospi['종목코드'][i]
+    a = dl.korea_stock(code, '2020-01-01', '2023-03-18')
+    kospi_list.append(a)
+    
+kosdaq_list = []
+
+for r in range(0,10):
+    code = kosdaq['종목코드'][r]
+    b = dl.korea_stock(code, '2020-01-01', '2023-03-18')
+    kosdaq_list.append(b)
+    
+    
+# DB 저장
+
+from sqlalchemy import create_engine
+con = create_engine('mysql+pymysql://root:django@3.34.50.194:54621/stock')
+kospi.to_sql('kospi',con, if_exists='replace')
+kosdaq.to_sql('kosdaq', con, if_exists = 'replace')
+
+# 신성델타테크 가격 정보 테이블에 넣기
+df = dl.korea_stock('065350', '2020-01-01', '2023-03-18')
+df.to_sql('ssdt', con, if_exists = 'replace')
+
+
+# DB에서 읽기
+import pandas as pd
+query = 'SELECT 회사명, 종목코드 FROM stock.kospi;'
+df = pd.read_sql(query, con)
+
