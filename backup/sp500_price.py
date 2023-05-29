64a58c7b11d932386d0bf5ac6bc893cd3d9a4268
joohoshin <finelloai@gmail.com>
2023-05-29 10:19:43 +0900

    Update sp500_price.py
    미국 데이터소스 확인

--- backup/sp500_price.py
+++ backup/sp500_price.py
@@ -1,9 +1,9 @@
-import pandas_datareader.data as web
+import FinanceDataReader as fdr
 
-import pandas as pd
+# S&P 500 종목 전체
+df_spx = fdr.StockListing('S&P500')
+df_spx.head()
 
-import datetime as dt
 
-df = web.DataReader('AAPL', 'yahoo')
-
-df.head()
+df = fdr.DataReader('AAPL', '2018-01-01', '2018-03-30')
+df.tail()
